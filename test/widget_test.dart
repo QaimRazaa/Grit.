9a149b1c79a577fb2a8f6842f2b328d98c2683ab
QAIM RAZA <qaimraza@QAIMs-MacBook-Air.local>
2026-03-24 10:28:27 +0500

    Folder Structure

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:grit/main.dart';
+import 'package:grit/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
